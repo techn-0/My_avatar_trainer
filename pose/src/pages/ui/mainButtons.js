ee425264c6e4e8c40b897dabb603914416fe7a0e
JIN JAE WOONG <johnjin56@gmail.com>
2024-10-21 16:27:55 +0900

    성장추이 리다이렉션 추가, 필요없는 버튼 제거

--- pose/src/pages/ui/mainButtons.js
+++ pose/src/pages/ui/mainButtons.js
@@ -24,8 +24,6 @@ function Buttons({
       <button onClick={onLoginPageClick}>로그인 페이지</button>
       <button onClick={handleExerciseClick}>운동하러 가기</button>
       <button onClick={handleResultClick}>성장 추이 보기</button>
-      <button onClick={onPlayIdleClick}>Play Idle Animation</button>
-      <button onClick={onPlayRunClick}>Play Run Animation</button>
     </div>
   );
 }
