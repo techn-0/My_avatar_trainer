44c1c733555d9484cbfbc102a6118e1c6f3e4cb2
Alisser <gnlrjswjd@naver.com>
2024-11-09 15:42:17 +0900

    랭킹 단상 버그픽스

--- pose/src/pages/ranking/ranking.js
+++ pose/src/pages/ranking/ranking.js
@@ -55,56 +55,58 @@ function RankingPage() {
 
   return (
     <div className="background">
-      <div className="rankin_main_btn_cnt">
-        <div className="radio-wrapper cyberpunk">
-          <input
-            className="input"
-            type="radio"
-            name="btn"
-            id="mainPage"
-            onClick={() => navigate("/")}
-            onMouseEnter={handleMouseEnter}
-          />
-          <div className="btn" onClick={() => navigate("/")}>
-            <span aria-hidden="true"></span>메인페이지
-            <span className="btn__glitch" aria-hidden="true">
-              메인페이지
-            </span>
+      <div>
+        <div className="rankin_main_btn_cnt">
+          <div className="radio-wrapper cyberpunk">
+            <input
+              className="input"
+              type="radio"
+              name="btn"
+              id="mainPage"
+              onClick={() => navigate("/")}
+              onMouseEnter={handleMouseEnter}
+            />
+            <div className="btn" onClick={() => navigate("/")}>
+              <span aria-hidden="true"></span>메인페이지
+              <span className="btn__glitch" aria-hidden="true">
+                메인페이지
+              </span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="ranking-page">
-        <div className="opt">
-          <div className="rankin_title">
-            <h2>랭킹</h2>
-            <div className="ranking_subtitle">
-              <label>
-                운동 종목:&nbsp;
-                <select
-                  value={exercise}
-                  onChange={(e) => setExercise(e.target.value)}
-                >
-                  <option value="squat">스쿼트</option>
-                  <option value="pushup">푸시업</option>
-                  <option value="plank">플랭크</option>
-                  <option value="situp">싯업</option>
-                  <option value="legraise">레그레이즈</option>
-                </select>
-              </label>
+        <div className="ranking-page">
+          <div className="opt">
+            <div className="rankin_title">
+              <h2>랭킹</h2>
+              <div className="ranking_subtitle">
+                <label>
+                  운동 종목:&nbsp;
+                  <select
+                    value={exercise}
+                    onChange={(e) => setExercise(e.target.value)}
+                  >
+                    <option value="squat">스쿼트</option>
+                    <option value="pushup">푸시업</option>
+                    <option value="plank">플랭크</option>
+                    <option value="situp">싯업</option>
+                    <option value="legraise">레그레이즈</option>
+                  </select>
+                </label>
 
-              <label>
-                운동 시간:&nbsp;
-                <select
-                  value={duration}
-                  onChange={(e) => setDuration(Number(e.target.value))}
-                >
-                  <option value={1}>1분</option>
-                  <option value={2}>2분</option>
-                  <option value={0.4}>24초</option>
-                  <option value={0.1}>6초</option>
-                </select>
-              </label>
+                <label>
+                  운동 시간:&nbsp;
+                  <select
+                    value={duration}
+                    onChange={(e) => setDuration(Number(e.target.value))}
+                  >
+                    <option value={1}>1분</option>
+                    <option value={2}>2분</option>
+                    <option value={0.4}>24초</option>
+                    <option value={0.1}>6초</option>
+                  </select>
+                </label>
+              </div>
             </div>
           </div>
         </div>
