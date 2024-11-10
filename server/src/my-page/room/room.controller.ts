1228984136bdb5faf01f3bc19f325016e89a4eeb
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2024-11-10 17:39:44 +0900

    Chatting function final edits added

--- server/src/my-page/room/room.controller.ts
+++ server/src/my-page/room/room.controller.ts
@@ -9,8 +9,9 @@ export class RoomController {
         private readonly messageService:MessageService
     ){}
 
-    @Post('join')
-    async getOrCreateRoom( @Body()body:{user1:string, user2:string}){
+    @Post('joinRoom')
+    async joinRoom( @Body()body:{user1:string, user2:string}){
+        //방이 있다면 방 정보를 가져오고, 없다면 방 정보를 형성한다.
         const {user1, user2} = body;
         const roomName = [user1, user2].sort().join('&');
         
@@ -19,7 +20,8 @@ export class RoomController {
         if(!room){
             throw new NotFoundException('Room not found!');
         }
-        
+
+        // 형성된 방의 메시지 정보를 가져온다.
         const messages = await this.messageService.getMessages(roomName);
 
         return {room, messages};
